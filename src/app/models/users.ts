8eac893cb27e5b9f68c150c966b86250ef73c705
arclps <aponcesanga@gmail.com>
2023-01-19 14:27:08 -0500

    commit

---
+++ src/app/models/users.ts
@@ -0,0 +1,10 @@
+export interface Users {
+  //creamos la interface con campos que existan en el json del api
+  // id: string;
+  // name: string;
+  // username: string;
+  userId: string;
+  id: string;
+  title: string;
+  completed: boolean;
+}
---
+++ src/app/principal/vista/vista.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { Users } from 'src/app/models/users';
+import { UserserviceService } from 'src/app/services/userservice.service';
+
+@Component({
+  selector: 'app-vista',
+  templateUrl: './vista.component.html',
+  styleUrls: ['./vista.component.css']
+})
+export class VistaComponent {
+
+
+  mostrarLi = false; //++++
+
+
+  //aqui creamos una instancia de userserviceservice
+  constructor(private userService:UserserviceService){}
+
+
+  //array que va a obtener toda la info del metodo getusersall
+  vistaListado = new Array(); // con ()
+
+  //ejecutar mmetodo getuserall al cargar el componente
+  // ngOnInit(){
+    mostrarTODOS(){
+    /* 
+    ejecutar metodo getUsersAll y obtener json devuelto por la api
+    this.userService.getUsersAll().subscribe({
+      next: (userAll:Users[])=> this.vistaListado=userAll, //el userAll dentro del () puede tener cualquier nombre, solo importa que sea de tipo Users[]
+      error:(error)=>console.error(error),
+      complete:()=> console.info('completed!')
+    }) */
+
+    //ejecutar el metodo getUsersAllInterceptor que captura la comunicacion, no un resultado
+    //de toda la comunicaion solo queremos el body (e json esta dentro del body)
+    this.userService.getUsersAllInterceptor().subscribe({
+      next: (response:any)=> {
+        this.vistaListado=response.body;
+        console.log(response);
+        this.mostrarLi =true; //+++++++++
+        this.ocultarLi=false;
+
+      },
+      error:(error)=>console.error(error),
+      complete:()=> console.info('completed!')
+    })
+
+  }
+
+  ocultarLi=false;
+  ocultarTODOS(){
+    // this.ocultarLi = !this.mostrarLi;
+    this.ocultarLi=true;
+  }
+
+}
